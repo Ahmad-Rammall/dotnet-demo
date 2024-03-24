3021926c5fa560e8419dde024b359ea2aec77203
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-03-25 00:53:25 +0200

    Create Category Model

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,5 +2,9 @@
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayOrder { get; set; }
+        public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
 }
