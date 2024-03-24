f6635bf54dd5acbf5e14ef3085fc438ed904ce90
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2024-03-25 00:57:34 +0200

    Add Category Data Annotations

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,10 +1,16 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Category
     {
+        [Key] // primary key
         public int Id { get; set; }
+
+        [Required] // Name is required
         public string Name { get; set; }
         public string DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
 }
+ 
